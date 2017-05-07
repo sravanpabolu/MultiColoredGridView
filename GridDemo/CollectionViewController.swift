b497b998544d98d2a68fd1037edc17cc5b828e42
Sravan <sravan@sravans-macbook-pro.local>
2017-05-07 21:10:36 +0530

    Initial Commit

---
+++ GridDemo/CollectionViewController.swift
@@ -0,0 +1,89 @@
+//
+//  CollectionViewController.swift
+//  GridDemo
+//
+//  Created by Sravan on 01/05/17.
+//  Copyright © 2017 Sravan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+    let reuseIdentifier = "Cell"
+    var rows = 15
+    var column = 8
+    let sectionInsets = UIEdgeInsets(top: 10.0, left: 10.0, bottom: 0.0, right: 10.0)
+}
+
+
+//MARK: Data source
+extension CollectionViewController {
+    //1
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return rows
+    }
+    
+    //2
+    override func collectionView(_ collectionView: UICollectionView,
+                                 numberOfItemsInSection section: Int) -> Int {
+        return column
+    }
+    
+    //3
+    override func collectionView(_ collectionView: UICollectionView,
+                                 cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier,
+                                                      for: indexPath)
+        cell.backgroundColor = .randomColor()
+        // Configure the cell
+        return cell
+    }
+}
+
+//MARK: Flow Layout Delegate
+extension CollectionViewController : UICollectionViewDelegateFlowLayout {
+    //1
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //2
+        let paddingSpace = sectionInsets.left * CGFloat(rows + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / CGFloat(rows)
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    //3
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    // 4
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+}
+
+
+//MARK: Other extensions
+extension CGFloat {
+    static func randomNumber() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+extension UIColor {
+    static func randomColor() -> UIColor {
+        return UIColor(red:   .randomNumber(),
+                       green: .randomNumber(),
+                       blue:  .randomNumber(),
+                       alpha: 1.0)
+    }
+}
+
---
+++ GridDemo/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  GridDemo
+//
+//  Created by Sravan on 14/04/17.
+//  Copyright © 2017 Sravan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var txtRows: UITextField!
+    @IBOutlet weak var tctColumn: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func btnSubmitTapped(sender: Any) {
+        var rows: Int = 1
+        var col: Int = 1
+        
+        if let row = txtRows.text {
+            rows = Int( row )!
+        }
+        
+        if let columnValue = tctColumn.text {
+            col = Int (columnValue)!
+        }
+        
+        let validationSuccess:Bool = validateTxtFields(rows: rows, Column: col)
+        
+        if validationSuccess == true {
+//            drawGrid(rows: rows, Column: col)
+            
+            let collectionObj: CollectionViewController = CollectionViewController()
+            collectionObj.rows = rows
+            collectionObj.column = col
+            
+            let secondViewController = self.storyboard?.instantiateViewController(withIdentifier: "CollectionViewController") as! CollectionViewController
+            self.navigationController?.pushViewController(secondViewController, animated: true)
+
+        }
+    }
+    
+    func validateTxtFields(rows: Int, Column:Int) -> Bool {
+        if rows > 0, Column > 0 {
+            return true
+        }
+        return false
+    }
+
+    func drawGrid(rows: Int, Column: Int) {
+        
+        for _ in 1...rows {
+            print("X")
+            for _ in 1...Column {
+                print("|")
+            }
+            
+        }
+    }
+ 
+
+}
+
+
